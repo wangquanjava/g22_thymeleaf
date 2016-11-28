1332b15aff6bd57f5222850494f04ca1d3524d50
wangquanjava <wangquanjava@163.com>
2016-11-28 11:13:39 +0800

    增加thymelef示例

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-# g05_ssm_template
-#### 1.需要修改的
-1. 修改pom.xml中artifactId、tomcat虚拟目录
-2. 修改web.xml中<display-name>、<servlet-name>
-3. 修改org.eclipse.wst.common.component中的项目名
-4. 修改jdbc.properties中数据库用户信息
-
-#### 2.自身功能
-4. 配置了一个默认的拦截器，拦截:/admin/** 默认允许 com.git.intercepor.DemoInteceptor，springmvc.xml
-5. 采用c3p0连接池
-6. 添加listener，用于监测服务器启动和关闭，web.xml\com.git.listener.ContextListener
-7. 配置pom.xml，支持服务器和本地开发的不用配置jdbc.properties、jdbc_product.properties、jdbc_development.properties
-8. 有全局异常类GlobeExceptionResolver，可以捕获controller中出现的错
-9. 支持通用mapper和sqlmapper两种查询方式
+# g22_thymeleaf
--- src/main/java/com/git/controller/DemoController.java
+++ src/main/java/com/git/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.git.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.git.domain.AjaxJson;
@@ -13,9 +14,10 @@ public class DemoController {
 	@Autowired
 	private DemoService demoService;
 	
-	@RequestMapping("get")
-	public ResponseEntity<AjaxJson> get(){
-		return ResponseEntity.status(200).body(null);
+	@RequestMapping("/home/xx")
+	public String get(Model model){
+		model.addAttribute("username", "王权");
+		return "home";
 	}
 	
 }
